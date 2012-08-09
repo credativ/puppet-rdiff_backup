1afeb80be86fe7c78cd0d1747373882fb626a8a1
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2012-08-09 12:08:15 +0200

    Add puppetdoc documentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,39 @@
+# = Class: rdiff_backup
+#
+# Manage backup jobs with rdiff-backup
+#
+# == Requirements:
+#
+# - This module makes use of the example42 functions in the puppi module
+#   (https://github.com/credativ/puppet-example42lib)
+#
+# - Uses the crond module to create cronjobs
+#   (https://github.com/credativ/crond)
+#
+# == Parameters:
+#
+# [*ensure*]
+#   Weither to ensure backups or not.
+#   Default: present
+#
+# [*base_backupdir*]
+#   Defines the base backup directory. Backups will be created in a hostname-
+#   specific subdirectory of this directory.
+#   (Default: /srv/configs/backup)
+#
+# [*remove_older_than*]
+#   Defines the maximum age of increments. Parameter needs to be accepted
+#   by the --remove-older-than option of rdiff-backup.
+#   (Default: 4W)
+#
+# [*include*]
+#   Defines the pathes to include. Its possible to specify excludes by just
+#   prefixing them with a - sign.
+#   (Default: ['/srv', '/etc'])
+#
+# == Author:
+#
+#  Patrick Schoenfeld (<patrick.schoenfeld@credativ.de>)
 class rdiff_backup (
     $ensure             = params_lookup('ensure'),
     $base_backupdir     = params_lookup('base_backupdir'),
@@ -18,22 +54,28 @@ class rdiff_backup (
         mode   => 0700,
     }
 
+    if $ensure == 'present' {
+        $ensure_backup_directory = directory
+    } else {
+        $ensure_backup_directory = $ensure
+    }
+
     file { '/etc/rdiff-backup':
-        ensure  => directory,
+        ensure  => $ensure,
         owner   => root,
         group   => root,
         mode    => 0644,
     }
 
     file { '/etc/rdiff-backup/includelist':
-        ensure => present,
+        ensure => $ensure,
         owner   => root,
         group   => root,
         content => template('rdiff_backup/includelist.erb')
     }
 
     crond { 'rdiff-backup':
-        ensure  => present,
+        ensure  => $ensure,
         command => [
             "rdiff-backup --include-globbing-filelist /etc/rdiff-backup/includelist / ${backupdir}",
             "rdiff-backup -v0 --remove-older-than $remove_older_than ${backupdir}"
